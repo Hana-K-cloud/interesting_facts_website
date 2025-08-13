f1b82450a0f8ffc790c4ab4c0879a3c60be01fed
my_git <hkh943354@gmail.com>
2025-08-13 10:01:37 +0330

    signup

--- my_app/urls.py
+++ my_app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from my_app import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('accounts/signup', views.signup, name='signup'),
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from .form import *
+from django.shortcuts import render, redirect
+
+
 
 # Create your views here.
 def home(request):
-    return render(request,'my_app/index.html')
+    return render(request, 'my_app/index.html')
+
+
+def signup(request):
+    # post method
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home', {'form': form})
+
+    # get method
+    form = SignupForm
+    return render(request, 'registration/signup.html', {'form': form})
